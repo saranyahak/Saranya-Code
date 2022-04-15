c969e364f4afe79aebbabd2282014818aa478839
saranyahak <103485044+saranyahak@users.noreply.github.com>
2022-04-15 13:08:23 +0530

    Update test_ksrtc.py

--- test_ksrtc.py
+++ test_ksrtc.py
@@ -4,15 +4,17 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 # from selenium.webdriver.common.by import by
 # from selenium.webdriver.support.select import select
-from selenium.webdriver.support.wait import WebDriverWait
-wait = WebDriverWait(driver,20)
+# from selenium.webdriver.support.wait import WebDriverWait
+# wait = WebDriverWait(driver,20)
 driver = webdriver.Chrome(ChromeDriverManager().install())
+# wait = WebDriverWait(driver,20)
 driver.get("https://ksrtc.in/oprs-web/")
 driver.implicitly_wait(10)
 driver.maximize_window()
 from_place=driver.find_element_by_id("fromPlaceName")
 from_place.send_keys("ben")
-# time.sleep(5)
-from_link = wait.until(EC visibility_of_element_located("//a[text()='BENGALURU']"))
+time.sleep(5)
+from_link =driver.find_element_by_xpath("//a[text()='BENGALURU']")
 from_link.click()
-# time.sleep(5)
+time.sleep(5)
+driver.close()
